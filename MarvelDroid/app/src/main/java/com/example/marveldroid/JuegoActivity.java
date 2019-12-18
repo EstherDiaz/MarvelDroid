cb8861e15f07d6b2b3a29ae25bde3ad0066cf054
estherdiaz <ediazd@fp.uoc.edu>
2019-12-18 22:16:08 +0100

    Buscar imagen en galería y pasarla a la siguiente activity (falla)

---
+++ MarvelDroid/app/src/main/java/com/example/marveldroid/JuegoActivity.java
@@ -0,0 +1,29 @@
+package com.example.marveldroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.ImageView;
+
+public class JuegoActivity extends AppCompatActivity {
+    private ImageView imaagenPuzle;
+    private String imagenUri;
+    public static final String KEY_IMAGEN = "IMAGEN";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_juego);
+        Intent intent = getIntent();
+        imagenUri = intent.getStringExtra(KEY_IMAGEN);
+        System.out.println(imagenUri);
+        inicializar();
+    }
+
+    private void inicializar() {
+        imaagenPuzle.setImageURI(Uri.parse(imagenUri) );
+    }
+}
--- MarvelDroid/app/src/main/java/com/example/marveldroid/JugarSinRegistro.java
+++ MarvelDroid/app/src/main/java/com/example/marveldroid/JugarSinRegistro.java
@@ -1,16 +1,25 @@
 package com.example.marveldroid;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import java.net.URI;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.provider.MediaStore;
+
 
 public class JugarSinRegistro extends AppCompatActivity {
 
     public static final String KEY_JUGAR = "JUGAR";
     private Button buttonBack;
+    private ImageButton botonImagenes;
+    private Bitmap guardarImagen;
+
 
 
     @Override
@@ -18,6 +27,8 @@ public class JugarSinRegistro extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jugar_sin_registro);
         buttonBack = findViewById(R.id.buttonBack);
+        botonImagenes = findViewById(R.id.botonImagenes);
+
         inicializar();
     }
 
@@ -33,5 +44,32 @@ public class JugarSinRegistro extends AppCompatActivity {
                 finish();
             }
         });
+
+        botonImagenes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CargarImagen();
+            }
+        });
+    }
+
+    private void CargarImagen(){
+        Intent intent=new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+//        intent.setType("image/");
+//        startActivityForResult(intent.createChooser(intent,"Seleccione la Aplicación"),10);
+        startActivityForResult(intent, 100);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode==RESULT_OK) {
+            Uri miPath = data.getData();
+            assert miPath != null;
+            guardarImagen = BitmapFactory.decodeFile(miPath.getPath());
+            Intent intent = new Intent(JugarSinRegistro.this, JuegoActivity.class);
+            intent.putExtra(JuegoActivity.KEY_IMAGEN, miPath.getPath());
+            startActivity(intent);
+        }
+
     }
 }
